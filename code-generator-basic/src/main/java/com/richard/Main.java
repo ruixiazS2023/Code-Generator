42142e63a8fa7a50fb5b3d7b7a988d8c4a853f4d
ruixiazS2023 <ruixiaz@andrew.cmu.edu>
2024-05-24 21:53:34 +0800

    add main and generate bash

--- code-generator-basic/src/main/java/com/richard/Main.java
+++ code-generator-basic/src/main/java/com/richard/Main.java
@@ -2,10 +2,6 @@ package com.richard;
 
 import com.richard.cli.CommandExecutor;
 
-import java.io.File;
-
-import static com.richard.generator.StaticGenerator.copyFilesByHutool;
-
 public class Main {
     public static void main(String[] args) {
         CommandExecutor commandExecutor = new CommandExecutor();
